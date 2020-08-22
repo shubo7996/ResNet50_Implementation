a149fa7b2a10672c4a818b8d8d8d928888a40f78
Subhamoy Paul <whatsupmofo420@gmail.com>
2020-08-23 03:20:12 +0530

    var name fix

--- process_model.py
+++ process_model.py
@@ -3,7 +3,7 @@ from ResNet50Model import ResNet50
 from keras.callbacks import CSVLogger
 import matplotlib.pyplot as plt
 
-X_test,Y_test,X_train,Y_train= DataSetProcessing.loadData()
+X_train_shuffled,Y_train_shuffled,X_test_shuffled,Y_test_shuffled= DataSetProcessing.loadData()
 model=ResNet50.ResNet50()
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -16,8 +16,9 @@ csv_logger=CSVLogger(
 
 model.fit(X_train_shuffled, Y_train_shuffled, epochs = 20, batch_size = 64,callbacks=[csv_logger])
 
-print ("Loss = " + str(preds[0]))
-print ("Test Accuracy = " + str(preds[1]))
+preds = model.evaluate(X_test_shuffled, Y_test_shuffled)
+print ("Loss="+str(preds[0]))
+print ("Test Accuracy="+str(preds[1]))
 
 
 filename='log.csv'
